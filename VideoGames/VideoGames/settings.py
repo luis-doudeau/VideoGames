09470fe4f9edba03b82231c9bdf10151a43fafc6
luis-doudeau <luis.doudeau@gmail.com>
2023-10-02 00:07:56 +0200

    admin et création model + migration

--- VideoGames/VideoGames/settings.py
+++ VideoGames/VideoGames/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'theGames',
+    'theGames.apps.ThegamesConfig',
+    'bootstrap5'
+
 ]
 
 MIDDLEWARE = [
@@ -116,6 +119,7 @@ DATE_INPUT_FORMATS = ('%d/%m/%Y', '%Y-%m-%d')
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- VideoGames/theGames/models.py
+++ VideoGames/theGames/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Game(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    release_date = models.DateTimeField('date published')
+    studio = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name + self.description + str(self.release_date) + self.studio
