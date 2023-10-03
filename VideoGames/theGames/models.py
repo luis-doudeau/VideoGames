db153d3abb43c39e638198752a272c76bdc63c1d
luis-doudeau <luis.doudeau@gmail.com>
2023-10-03 18:06:12 +0200

    ajout d'une première vue pour afficher les jeux avec template générique

--- VideoGames/theGames/models.py
+++ VideoGames/theGames/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Game(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
-    release_date = models.DateTimeField('date published')
+    release_date = models.DateTimeField(auto_now_add=True)
     studio = models.CharField(max_length=200)
 
     def __str__(self):
--- VideoGames/theGames/views.py
+++ VideoGames/theGames/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Game
 
 
 def home(request):
     return HttpResponse('bonjour à tous')
+
+
+def game_listing(request):
+    games=Game.objects.all()
+    return render(request, template_name='list_games.html', context={'games':games})
