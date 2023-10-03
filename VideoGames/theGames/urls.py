aae0d85921440819b101ff560ddcf3eb8eabf25c
luis-doudeau <luis.doudeau@gmail.com>
2023-10-03 18:29:11 +0200

    debut du CRUD

--- VideoGames/theGames/urls.py
+++ VideoGames/theGames/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('home/', views.home, name='home'),
     path('games/', views.game_listing, name='games'),
+    path('games/<id>', views.game_detail, name='game'),
+    path("games/form", views.gameForm, name="game_form")
+
 ]
--- VideoGames/theGames/views.py
+++ VideoGames/theGames/views.py
@@ -10,3 +10,44 @@ def home(request):
 def game_listing(request):
     games=Game.objects.all()
     return render(request, template_name='list_games.html', context={'games':games})
+
+def game_detail(request, id):
+    try : 
+     game=Game.objects.get(pk=id)
+    except Game.DoesNotExist : 
+          raise Http404("Game does not exist")
+    return render(request, template_name='game.html', context={'game':game})
+
+
+def gameForm(request):
+    gameForm = TaskForm()
+    # on teste si on est bien en validation de formulaire (POST)
+    if request.method == "POST":
+        # Si oui on récupère les données postées
+        form = TaskForm(request.POST)
+        # on vérifie la validité du formulaire
+        if form.is_valid():
+            new_game = form.save()
+            context = {'game': new_game}
+            return render(request,'game.html', context)
+    # Si méthode GET, on présente le formulaire
+    context = {'game': gameForm}
+
+    return render(request,'game_form.html', context)
+    
+
+# def my_task_delete(request) :
+#     taskform = TaskForm()
+#     # on teste si on est bien en validation de formulaire (POST)
+#     if request.method == "DELETE":
+#         # Si oui on récupère les données postées
+#         form = TaskForm(request.POST)
+#         # on vérifie la validité du formulaire
+#         if form.is_valid():
+#             new_task = form.save()
+#             context = {'task': new_task}
+#             return render(request,'task.html', context)
+#     # Si méthode GET, on présente le formulaire
+#     context = {'form': taskform}
+
+#     return render(request,'mytaskdelete.html', context)
